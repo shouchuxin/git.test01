9922f55c4db07b7fe942e753f2ab5df4d26e2175
初心 <1368547310@qq.com>
2022-03-20 09:50:40 +0800

    hot-fix commit

--- src/main/java/com/chuxin/git/Git/Test.java
+++ src/main/java/com/chuxin/git/Git/Test.java
@@ -3,5 +3,7 @@ package com.chuxin.git.Git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello chuxin");
+
     }
 }
